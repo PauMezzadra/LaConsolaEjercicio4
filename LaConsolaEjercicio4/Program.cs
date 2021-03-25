949e6a383ef3e742b72710751e6f2d273b40d783
Pau Mezzadra <paumezzadra@hotmail.com>
2021-03-25 14:16:16 -0300

    Add project files.

---
+++ LaConsolaEjercicio4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaConsolaEjercicio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string valor = "";
+            do
+            {
+                valor = ServValidac.PedirStringNoVacio("Ingrese cualquier frase, para finalizar ingrese ´FIN´");
+                Console.WriteLine("Usted ingresó la frase: " + valor);
+
+            } while (valor != "FIN");
+        }
+    }
+}
---
+++ LaConsolaEjercicio4/ServValidac.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaConsolaEjercicio4
+{
+    static class ServValidac
+    {
+        public static string PedirStringNoVacio (string mensaje)
+        {
+            string valor = "";
+            do
+            {
+                Console.WriteLine(mensaje);
+                valor = Console.ReadLine().ToUpper();
+                if (valor == "")
+                {
+                    Console.WriteLine("No puede ser vacío");
+                }
+            } while (valor == "");
+            
+            return valor;
+        }
+    }
+}
